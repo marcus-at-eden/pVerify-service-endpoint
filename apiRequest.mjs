2c040485b88083a3ffd95ae769ab53f4a9bbb8c7
Marcus Roldan <marcus@edenonearth.ai>
2025-05-05 16:48:15 -0400

    updated to utilize new auth bearer tokens

--- apiRequest.mjs
+++ apiRequest.mjs
@@ -1,18 +1,48 @@
+async function getBearerToken() {
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("Client_Id", process.env.clientApiID);
+  urlencoded.append("Client_Secret", process.env.clientSecret);
+  urlencoded.append("grant_type", "client_credentials");
+  urlencoded.append("", "");
+
+  var requestOptions = {
+    method: 'POST',
+    body: urlencoded,
+    redirect: 'follow'
+  };
+  try {
+    const response = await fetch("https://api.pverify.com/Token", requestOptions);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching bearer token:", error);
+    throw error; 
+  }
+}
+
 export async function makePVerifyRequest(endpoint, payload, debug) {
-  const clientApiId = process.env.clientApiID;
-  const clientSecret = process.env.clientSecret;
+
+  // First get the bearer token from pVerify
+  bearerTokenResponse = await getBearerToken();
+  token = bearerTokenResponse.access_token;
 
   const reqHeaders = {
     "Content-Type": "application/json",
-    "Client-Secret": clientSecret,
-    "Client-API-Id": clientApiId,
+    "Authorization": `Bearer ${token}`,
+    "Client-API-Id": process.env.clientApiID
   };
 
   const request = {
     method: "POST",
     headers: reqHeaders,
     body: JSON.stringify(payload),
-  }; 
+  };
 
   // If the debug flag is set, do not execute the request, but log it instead.
   if (debug) {
