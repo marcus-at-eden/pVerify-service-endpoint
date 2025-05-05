b97816350272818415794e96bf750fad94565f28
Marcus Roldan <marcus@edenonearth.ai>
2025-05-05 12:51:46 -0400

    refactored to handle debug mode and prevent extraneous pVerify request from being sent

--- apiRequest.mjs
+++ apiRequest.mjs
@@ -1,17 +1,22 @@
-export async function makePVerifyRequest(payload) {
+export async function makePVerifyRequest(payload, debug) {
   const endpoint = process.env.endpoint;
   const clientApiId = process.env.clientApiID;
   const clientSecret = process.env.clientSecret;
 
-  const headers = {
+  const reqHeaders = {
     "Content-Type": "application/json",
     "Client-Secret": clientSecret,
     "Client-API-Id": clientApiId,
   };
 
+  // If the debug flag is set, do not execute the request, but log it instead.
+  if (debug) {
+    console.log(`Debug flag set. \n Request to pVerify: ${{method: "POST", headers: reqHeaders, body: JSON.stringify(payload)}}`);
+  }
+
   const response = await fetch(endpoint, {
     method: "POST",
-    headers: headers,
+    headers: reqHeaders,
     body: JSON.stringify(payload),
   });
 
