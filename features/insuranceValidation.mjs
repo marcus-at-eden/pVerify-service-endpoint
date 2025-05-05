1b9b895de8af5805ad7ada289f62e984e864bcab
Marcus Roldan <marcus@edenonearth.ai>
2025-05-05 13:24:35 -0400

    added imports

--- features/insuranceValidation.mjs
+++ features/insuranceValidation.mjs
@@ -1,3 +1,6 @@
+import { makePVerifyRequest } from "../apiRequest.mjs";
+import { processInsuranceVerificationResponse } from "../processPVerifyResponse.mjs";
+
 export async function handleInsuranceValidation(payloadArgs, debug) {
 
     const pVerifyResponse = await makePVerifyRequest(process.env.insuranceValidationEndpoint, payloadArgs, debug);
