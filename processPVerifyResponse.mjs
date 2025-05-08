9b584c40a40d67320a70678caeb565cd9a0e4807
Marcus Roldan <marcus@edenonearth.ai>
2025-05-08 17:56:33 -0400

    wrote function to unpack the insuranceVerification resposne object from pVerify

--- processPVerifyResponse.mjs
+++ processPVerifyResponse.mjs
@@ -1,3 +1,76 @@
 export async function processInsuranceVerificationResponse(pVerifyResponse) {
-    return {};
+    // Check for unsuccessful response
+    if (pVerifyResponse.APIResponseCode != "0"){
+        return {
+            success: false,
+            message: `Unsuccessful pVerify response: ${pVerifyResponse.ErrorCode} - ${pVerifyResponse.ErrorDescription}`,
+            statusCode: 1
+        }
+    }
+
+    var parsedResponseObject = {
+        payerName: pVerifyResponse.PayerName,
+        payerCode: pVerifyResponse.PayerCode,
+        dateOfService: pVerifyResponse.DOS,
+        practiceDescription: pVerifyResponse.ResultPracticeType,
+        priorAuthenticationRequired: pVerifyResponse.IsPriorAuthRequired,
+        subscriberInformation: pVerifyResponse.DemographicInfo.Subscriber,
+        dependentInformation: pVerifyResponse.DemographicInfo.Dependent,
+        otherPayerInformation: pVerifyResponse.OtherPayerInfo,
+        pcpAuthorizationInformationSummery: pVerifyResponse.PCPAuthInfoSummary,
+        medicareInformation: pVerifyResponse.MedicareInfoSummary,
+        miscInformation: pVerifyResponse.MiscellaneousInfoSummary,
+        specialistOfficeInformation: pVerifyResponse.SpecialistOfficeSummary,
+        copayInformation: {
+            inNetwork: pVerifyResponse.SpecialistOfficeSummary.CoPayInNet,
+            outNetwork: pVerifyResponse.SpecialistOfficeSummary.CoPayOutNet
+        }
+    };
+
+    // Look for any specialist fields that are not null
+    const additionalFields = [
+        "VisionOptometrySummary",
+        "PharmacySummary",
+        "HospitalOutPatientSummary",
+        "DentalSummary",
+        "TelemedicinePrimaryCareSummary",
+        "TelemedicineSpecialistSummary",
+        "TelemedicineUrgentCareSummary",
+        "TelemedicinePhysicalTherapySummary",
+        "TelemedicineMentalHealthSummary",
+        "PsychotherapySummary",
+        "SNFSummary",
+        "SNFRoomBoardSummary",
+        "ProfessionalPhysicianVisitInpatientSummary",
+        "HomeHealthCareSummary",
+        "HospiceSummary",
+        "AllergiesSummary",
+        "DiagnosticMedicalSummary",
+        "MaternitySummary",
+        "ImmunizationsSummary",
+        "TelemedicineOccupationalTherapySummary",
+        "TelemedicineSpeechTherapySummary",
+        "RadiationTherapySummary",
+        "InfertilitySummary",
+        "AllergyTestingSummary",
+        "InvitroFertilizationSummary",
+        "PsychiatricSummary",
+        "AcupunctureSummary",
+        "GynecologicalSummary",
+        "ObstetricalSummary",
+        "ObstetricalGynecologicalSummary",
+        "FamilyPlanningSummary",
+        "SurgicalBenefitsProfessionalPhysicianSummary",
+        "SurgicalBenefitsFacilitySummary",
+        "DialysisSummary",
+        "MentalHealthFacilityOutPatientSummary",
+        "IsCptInfoUpdated",
+        "PreventiveServices"
+    ];
+
+    additionalFields.forEach(field => {
+        if (pVerifyResponse[field] != null) {
+            parsedResponseObject[field] = pVerifyResponse[field];
+        }
+    });
 }
