4bdc04891e4d8e1c20700c411ae8a962c5a61521
Marcus Roldan <marcus@edenonearth.ai>
2025-05-08 21:05:44 -0400

    removed old comment with parsing

--- features/insuranceVerification.mjs
+++ features/insuranceVerification.mjs
@@ -45,36 +45,4 @@ export async function handleInsuranceVerification(payloadArgs, payers, debug) {
 
   const pVerifyResponse = await makePVerifyRequest(process.env.insuranceVerificationEndpoint, pVerifyPayload, debug);
   return processInsuranceVerificationResponse(pVerifyResponse);
-
-  // let copayPrimary = null;
-  // let copaySpecialist = null;
-
-  // const serviceDetails = data.ServiceDetails || [];
-  // for (const service of serviceDetails) {
-  //   if (service.ServiceName === "Professional(Physician) Visit - Office") {
-  //     const details = service.EligibilityDetails || [];
-  //     for (const detail of details) {
-  //       if (
-  //         detail.EligibilityOrBenefit === "Co-Payment" &&
-  //         Array.isArray(detail.Message)
-  //       ) {
-  //         const message = detail.Message.join(" ").toLowerCase();
-  //         if (message.includes("primary care visit") && !copayPrimary) {
-  //           copayPrimary = detail.MonetaryAmount;
-  //         }
-  //         if (message.includes("specialist visit") && !copaySpecialist) {
-  //           copaySpecialist = detail.MonetaryAmount;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  // return {
-  //   Eligibility: data.PlanCoverageSummary?.Status || "Inactive",
-  //   CoPayPrimaryCare: copayPrimary,
-  //   CoPaySpecialist: copaySpecialist,
-  //   ApiNotice: data.APIResponseMessage || "",
-  //   RawDetails: data,
-  // };
 }
